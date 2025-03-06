43ad59365738b3b19467a66731fc4002647effe4
xpr <168950241+xprshh@users.noreply.github.com>
2025-03-06 16:39:02 -0500

    too much gtk on my mind

--- home-manager/emacs.nix
+++ home-manager/emacs.nix
@@ -1,9 +1,31 @@
 {
   programs.emacs = {
     enable = true;
-    package = pkgs.emacs;  # replace with pkgs.emacs-gtk, or a version provided by the community overlay if desired.
+    package = pkgs.emacs-gtk;  # You can replace this with pkgs.emacs-gtk or another variant.
+
     extraConfig = ''
-      (setq standard-indent 2)
+      ;; Set up package.el to work with MELPA, GNU, and NonGNU
+      (require 'package)
+      (setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                               ("gnu" . "https://elpa.gnu.org/packages/")
+                               ("nongnu" . "https://elpa.nongnu.org/nongnu/")))
+      (package-initialize)
+
+      ;; Refresh package contents only if necessary
+      (unless package-archive-contents
+        (package-refresh-contents))
+
+      ;; Install use-package if not already installed
+      (unless (package-installed-p 'use-package)
+        (package-install 'use-package))
+
+      (require 'use-package)
+      (setq use-package-always-ensure t)
+
+      ;; Install and enable Evil mode
+      (use-package evil
+        :config
+        (evil-mode 1))
     '';
   };
 }
