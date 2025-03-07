e46491634edd5cfbddbac5da81f73641e02c0577
xpr <168950241+xprshh@users.noreply.github.com>
2025-03-06 19:43:03 -0500

    Update nixos.nix

--- nixos/nixos.nix
+++ nixos/nixos.nix
@@ -60,7 +60,6 @@ in {
         ../home-manager/theme.nix
         ../home-manager/tmux.nix
         ../home-manager/wezterm.nix
-        ../home-manager/emacs.nix
         ./home.nix
       ];
     };
